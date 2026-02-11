99698a1d7822b16769ec2ec3ea4bb042d578632a
kaushik1719 <kaushikkanani1719@gmail.com>
2026-02-11 09:03:19 +0530

    Fixed base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/kaushik_portfolioV1/',  // e.g., '/myportfolio/'
+  base: '/',   // ✅ FIXED
 })
